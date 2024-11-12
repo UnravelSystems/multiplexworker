5aff82117914b70927bd60265a59d46c275df64e
Dev <dev@unravel.systems>
2024-11-12 11:11:00 -0500

    RabbitMQ
    Moving more files around
    Adding configurations for using RabbitMQ instead of the local in memory queue

---
+++ S3RabbitMongo/Configuration/Database/Mongo/MongoDocumentStoreOptions.cs
@@ -0,0 +1,8 @@
+﻿namespace S3RabbitMongo.Configuration.Database.External;
+
+[OptionsConfiguration(ServiceName = "document_store.mongo")]
+public class MongoDocumentStoreOptions
+{
+    public string Collection { get; set; }
+    public string Database { get; set; }
+}
